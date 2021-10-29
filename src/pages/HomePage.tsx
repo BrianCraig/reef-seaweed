282a70ef39a8f76b11d1cbeea7722fb802d1cf35
Brian Craig <briancraigok@gmail.com>
2021-10-29 18:41:09 -0300

    feat: light/dark mode button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,11 @@
-import { Heading, Stack } from "@chakra-ui/react"
+import { Button, Heading, Stack, useColorMode } from "@chakra-ui/react"
 
 export const HomePage = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
   return <Stack spacing={8}>
     <Heading>Home Page</Heading>
+    <Button onClick={toggleColorMode}>
+      Toggle {colorMode === "light" ? "Dark" : "Light"}
+    </Button>
   </Stack>
 }
