a7890244d32c70f017511b511db03f5b1479beb6
Brian Craig <briancraigok@gmail.com>
2021-10-29 18:41:27 -0300

    feat: simple ido summary

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,6 +1,26 @@
-import { Heading, Stack } from "@chakra-ui/react"
+import { Avatar, Heading, SimpleGrid, Stack, Text } from "@chakra-ui/react"
+
+const IDOSummary = () => {
+  return <Stack border={"1px"} borderColor={"app.400"} borderRadius={8} bg="app.50" spacing={2} padding={4}>
+    <Stack direction={"row"} spacing={4}>
+      <Avatar size={"md"} name="Y K" />
+      <Stack justifyContent={"space-between"} spacing={0}>
+        <Heading size={"md"}>Project title</Heading>
+        <Text>Project subtitle</Text>
+      </Stack>
+    </Stack>
+  </Stack >
+}
+
 export const ProjectsPage = () => {
   return <Stack spacing={8}>
-    <Heading>Projects</Heading>
+    <Heading>Open projects</Heading>
+    <SimpleGrid columns={3} spacing={8}>
+      <IDOSummary />
+      <IDOSummary />
+      <IDOSummary />
+      <IDOSummary />
+      <IDOSummary />
+    </SimpleGrid>
   </Stack>
 }
