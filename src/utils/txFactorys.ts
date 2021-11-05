6932b29fc2108ebfa2206a411c0e819be394e05d
Brian Craig <briancraigok@gmail.com>
2021-11-05 09:19:51 -0300

    feat: remove bn.js dep

--- src/utils/txFactorys.ts
+++ src/utils/txFactorys.ts
@@ -2,7 +2,6 @@
 import { Provider } from '@reef-defi/evm-provider';
 import { AccountSigner, PublishValues, VestingForm } from '../utils/types';
 import { ensure, ratioToMulDiv } from './utils';
-import BN from "bn.js"
 import { utils } from "ethers";
 import { MAX_VESTING_OCURRENCES } from '../abis/contracts';
 
@@ -44,36 +43,6 @@ export const ClaimEvmTx: TxType = {
     const hasEvmAddress = await signer.signer.isClaimed();
     ensure(!hasEvmAddress, 'Account already has EVM address!');
     await signer.signer.claimDefaultAccount();
-  }
-}
-
-export const TransferTx: TxType = {
-  title: "Transfer REEF",
-  fields: [
-    {
-      name: "address",
-      type: FieldType.Address
-    },
-    {
-      name: "quantity",
-      type: FieldType.Amount
-    }
-  ],
-  action: async ({ signer, provider, params: { quantity, address } }) => {
-    let q = new BN((parseFloat(quantity) * 1e5).toFixed(0)).mul(new BN("10000000000000"));
-    let unsub = await provider.api.tx.balances
-      .transfer(address, q)
-      .signAndSend(signer.address, ({ events = [], status }) => {
-        console.log(`Current status is ${status.type}`);
-        if (status.isFinalized) {
-          console.log(`Transaction included at blockHash ${status.asFinalized}`);
-          events.forEach(({ phase, event: { data, method, section } }) => {
-            console.log(`\t' ${phase}: ${section}.${method}:: ${data}`);
-          });
-
-          unsub();
-        }
-      });
   }
 }
 
